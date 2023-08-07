9e102c61fc9f0e23f778a345412b1f1759a7a34f
Arifa Muhammad Syabani <arifamuhammads@gmail.com>
2023-08-07 21:42:18 +0700

    add security

--- src/main/java/com/loginspringbootv2/demo/service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/loginspringbootv2/demo/service/ServiceImpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.loginspringbootv2.demo.service.ServiceImpl;
 
+import com.loginspringbootv2.demo.exception.ResourceNotFoundException;
 import com.loginspringbootv2.demo.models.User;
 import com.loginspringbootv2.demo.repository.UserRepository;
 import com.loginspringbootv2.demo.service.UserService;
@@ -19,5 +20,13 @@ public class UserServiceImpl implements UserService {
     public User saveUser(User user){return userRepository.save(user);}
 
     @Override
-    public User getUserByUsername(String username){ return userRepository.findByUsername(username);}
+    public User getUserByUsername(String username){
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new ResourceNotFoundException("User","username",username);
+        }
+        else {
+            return userRepository.findByUsername(username);
+        }
+    }
 }
